c8cc246997d7f23329eac1ad3d50b469ef803513
giiacom <g.petracin@gmail.com>
2025-02-07 13:06:02 +0100

    database atlas

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+/*
 // Function to connect to MongoDB
 const connect = () => {
   mongoose.connect('mongodb://127.0.0.1:27017/provadb', {
@@ -14,4 +14,15 @@ const connect = () => {
   });
 };
 
+module.exports = { connect };*/
+require('dotenv').config(); // Load env variables
+
+const connect = () => {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+};
 module.exports = { connect };
